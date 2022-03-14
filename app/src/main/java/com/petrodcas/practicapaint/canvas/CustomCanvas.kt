35980d27d1e2b0e3e33852a9eb2220d4fa1e91b4
Peter Rodríguez Casado <petrodcas@gmail.com>
2022-03-14 02:42:25 +0100

    Arreglado el problema con el CustomCanvas: Ahora puede inicializarse y la aplicación funciona con él.

--- app/src/main/java/com/petrodcas/practicapaint/canvas/CustomCanvas.kt
+++ app/src/main/java/com/petrodcas/practicapaint/canvas/CustomCanvas.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
-class CustomCanvas(context: Context, attributes: AttributeSet? = null,
+class CustomCanvas @JvmOverloads constructor(context: Context, attributes: AttributeSet? = null,
                    defStyleAttr: Int = 0) : View(context, attributes, defStyleAttr) {
 
 }
