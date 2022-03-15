7dabdae6684fab301711d9c68bdce50ad43c73c4
Peter Rodríguez Casado <petrodcas@gmail.com>
2022-03-15 08:49:34 +0100

    Terminada la clase CustomCanvas

--- app/src/main/java/com/petrodcas/practicapaint/canvas/CustomCanvas.kt
+++ app/src/main/java/com/petrodcas/practicapaint/canvas/CustomCanvas.kt
@@ -10,6 +10,10 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
 import com.petrodcas.practicapaint.R
+import java.lang.Float.max
+import java.lang.Float.min
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 class CustomCanvas @JvmOverloads constructor(
     context: Context, attributes: AttributeSet? = null,
@@ -31,9 +35,9 @@ class CustomCanvas @JvmOverloads constructor(
         const val DEFAULT_STROKE_COLOR = R.color.black
         const val MIN_STROKE_WIDTH = 1f
         const val MAX_STROKE_WIDTH = 255f
-        const val DEFAULT_STROKE_WIDTH = 60f
+        const val DEFAULT_STROKE_WIDTH = 30f
         const val DEFAULT_FILL = false
-        val DEFAULT_MODE = Mode.BRUSH
+        val DEFAULT_MODE = Mode.RECTANGLE
     }
 
 
@@ -41,7 +45,7 @@ class CustomCanvas @JvmOverloads constructor(
     data class Action (
         val strokeWidth: Float? = null,
         var strokeColor: Int? = null,
-        val isFilled: Boolean? = null,
+        val isFilled: Boolean = false,
         val prevBGColor: Int? = null,
         val path: Path? = null,
         val mode: Mode = DEFAULT_MODE
@@ -107,7 +111,7 @@ class CustomCanvas @JvmOverloads constructor(
             isAntiAlias = true
             // Dithering affects how colors with higher-precision than the device are down-sampled.
             isDither = true
-            style = Paint.Style.STROKE // default: FILL
+            style = determineStrokeStyle() // default: FILL
             strokeJoin = Paint.Join.ROUND // default: MITER
             strokeCap = Paint.Cap.ROUND // default: BUTT
             strokeWidth = config.strokeWidth!!// default: Hairline-width (really thin)
@@ -121,14 +125,11 @@ class CustomCanvas @JvmOverloads constructor(
 
 
     override fun onDraw(canvas: Canvas) {
-        Log.d(":::onDraw", "se ha entrado al método")
         super.onDraw(canvas)
         //se pinta el fondo del bitmap al del color de fondo actual
         canvas.drawColor(config.bgColor!!)
-        Log.d(":::onDraw", "Se intentan dibujar los elementos")
         //se dibujan los elementos pintados hasta el momento
         drawPath(canvas)
-        Log.d(":::onDraw", "saliendo del método")
     }
 
 
@@ -140,94 +141,166 @@ class CustomCanvas @JvmOverloads constructor(
      */
     private fun drawPath (canvas: Canvas) {
 
-        //por cada trío de datos en la data class Action almacenada en la cola de "deshacer" (acciones realizadas hasta el momento)
-        for (a: Action in config.undoQueue!!) {
-            //Log.d(":::drawPath", "Se procede a filtrar por modo.")
-            //realiza acciones según el modo de la acción
+        //Dibuja cada elemento existente en undoQueue según los datos guardados.
+        for (a:Action in config.undoQueue!!) {
             when (a.mode) {
-                //si es modo el borrador, entonces cambia el color de su pintura al del color de fondo actual y después dibuja
-                Mode.ERASER -> {
-                    a.strokeColor = config.bgColor!!
+                //si era modo cambiar de color el fondo, no hace falta hacer nada aquí (actualmente ya existe dibujado el color de fondo más reciente)
+                Mode.FILL_BG -> continue
+                else -> {
                     updatePaint(a)
                     canvas.drawPath(a.path!!, usedPaint)
-                }//si era modo cambiar de color el fondo, no hace falta hacer nada aquí (actualmente ya existe dibujado el color de fondo más reciente)
-                Mode.FILL_BG -> continue
-                else -> {//para cualquier otro caso, dibuja lo que corresponda tal y como se hizo en su momento
-                    //Log.d(":::drawPath", "Se determina que se trata de un else. Se procede a dibujarlo en el canvas")
-                    updatePaint(a)
-                    canvas.drawPath(a.path!!, usedPaint!!)
                 }
-
-            }//fin del when
-
-        }//fin del for
-
-        //dibuja el path actual
+            }
+        }
+        //se dibuja el path actual, pues no se añade a la cola hasta haberse terminado el evento de toque
         updatePaint()
         canvas.drawPath(touchPath, usedPaint)
 
-    }//fin de la función
-
-
-
+    }
 
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        //actua acorde al tipo de evento recibido
         when(event.action) {
             MotionEvent.ACTION_DOWN -> onTouchStart(event.x, event.y)
             MotionEvent.ACTION_MOVE -> onTouchMove(event.x, event.y)
             MotionEvent.ACTION_UP -> onTouchEnd(event.x, event.y)
         }
+        //solo actualiza el canvas si se ha dibujado algo. Es una medida de optimización.
         if (drawingDone) invalidate()
         return true
     }
 
 
+    /**
+     * Gestiona qué ocurre cuando el usuario toca la pantalla
+     */
     private fun onTouchStart(newX: Float, newY: Float) {
-        //Log.d(":::onTouchStart", "Se accede al método")
+        //se mueve el comienzo del path a donde se tocó y se actualizan los coordenadas actuales
         touchPath.moveTo(newX, newY)
         currentX = newX
         currentY = newY
-        //Log.d(":::onTouchStart", "Se sale del método")
     }
 
+    /**
+     * Gestiona qué ocurre cuando el usuario mueve el dedo (o lo que sea con que esté tocando la pantalla).
+     *
+     * @param newX Nuevo valor de la coordenada X. Es decir, donde el usuario ha parado el dedo.
+     * @param newY Nuevo valor de la coordenada Y. Es decir, donde el usuario ha parado el dedo.
+     */
     private fun onTouchMove(newX: Float, newY: Float) {
-        //Log.d(":::onTouchMove", "Se accede al método")
+        //se calculan las diferencias entre los valores nuevos y viejos de las coordenadas
+        // para saber la distancia que se he desplazado en la pantalla
         val deltaX = Math.abs(newX - currentX)
         val deltaY = Math.abs(newY - currentY)
 
+        //si se supera la tolerancia de toque, entonces se puede dibujar, si no, no
         if (deltaX >= touchTolerance || deltaY >= touchTolerance) {
-            //Log.d(":::onTouchMove", "Se ha superado la tolerancia")
-            touchPath.quadTo(
-                currentX,
-                currentY,
-                (currentX + newX)/2f,
-                (currentY + newY)/2f
-            )
-            currentX = newX
-            currentY = newY
-            drawingDone = true
+            Log.d(":::onTouchMove", "Se ha superado la tolerancia")
+            when (config.mode) {
+                Mode.BRUSH, Mode.ERASER -> {
+                    touchPath.quadTo(
+                        currentX,
+                        currentY,
+                        (currentX + newX)/2f,
+                        (currentY + newY)/2f
+                    )
+                    currentX = newX
+                    currentY = newY
+                    drawingDone = true
+                }
+                Mode.RECTANGLE -> drawRectangle(newX, newY)
+                Mode.CIRCLE -> drawCircle(newX, newY)
+                else -> return //no se hace nada
+            }
         }
-        //Log.d(":::onTouchMove", "Se sale del método")
     }
 
+
+    /**
+     * Dibuja un rectángulo desde su lado izquierdo superior
+     */
+    private fun drawRectangle (newX: Float, newY: Float) {
+        //es necesario reiniciar el path para no crear múltiples rectángulos
+        touchPath.reset()
+
+        val left = min(currentX, newX)
+        val right = max(currentX, newX)
+        val bottom = max(currentY, newY)
+        val top = min(currentY, newY)
+
+        touchPath.addRect(left, top, right, bottom, Path.Direction.CW)
+
+        drawingDone = true
+    }
+
+
+    /**
+     * Dibuja un círculo perfecto centrado en el punto clickado
+     */
+    private fun drawCircle (newX: Float, newY: Float) {
+        //se resetea el path para que no se dibujen múltiples círculos
+        touchPath.reset()
+
+        val dx = abs(newX - currentX)
+        val dy= abs(newY - currentY)
+        val radius = sqrt((dx*dx) + (dy*dy) ) / 2f
+
+        touchPath.addCircle(currentX, currentY, radius, Path.Direction.CW)
+
+        drawingDone = true
+    }
+
+
+    /**
+     * Dibuja un óvalo desde su lado izquierdo
+     */
+    private fun drawOval (newX: Float, newY: Float) {
+        //se resetea el path para que no se dibujen múltiples óvalos
+        touchPath.reset()
+
+        val left = min(currentX, newX)
+        val top = min (currentY, newY)
+        val right = max (currentX, newX)
+        val bottom = max (currentY, newY)
+
+        touchPath.addArc(left, top, right, bottom, 0f, 360f)
+
+        drawingDone = true
+    }
+
+    /**
+     * Gestiona lo que ocurre cuando el usuario levanta el dedo de la pantalla tras un evento de toque
+     */
     private fun onTouchEnd(newX: Float, newY: Float) {
-        //Log.d(":::onTouchEnd", "Se accede al método")
+        //si se dibujó algo, entonces añade un nuevo Action a la cola de cosas hechas
         if (drawingDone) {
-            config.undoQueue!!.addLast(Action(strokeWidth = config.strokeWidth, strokeColor =  config.strokeColor, isFilled = config.isFilled, path = Path(touchPath), mode = config.mode!!))
+            config.undoQueue!!.addLast(generateActionFromConfig())
         }
+        //limpia la cola de rehacer
         config.redoQueue!!.clear()
+        //restablece los valores de las coordenadas x e y actuales
         currentY = 0f
         currentX = 0f
+        //restablece el valor de si se ha dibujado
         drawingDone = false
+        //reinicia el path para reutilizarlo: mayor eficiencia
         touchPath.reset()
-        //Log.d(":::onTouchEnd", "Se sale del método")
     }
+
 
 
 
     /**
-     * Establece los valores de la configuración del canvas en base a los valores recibidos
+     * Establece los valores de la configuración del canvas en base a los valores recibidos.
+     *
+     * Los valores nulos son ignorados, por lo que puede usarse este método para establecer casi
+     * cualquier atributo.
+     *
+     * No obstante, no se debe utilizar esta opción para cambiar el valor de [ConfigurationOptions.bgColor] a
+     * menos que sea para inicializar los valores a los de un canvas anterior.
+     *
+     * Si se desea cambiar el color del fondo, se debe usar el método [tintBackground].
      */
     fun setConfig (options: ConfigurationOptions) {
         if (options.undoQueue != null) config.undoQueue = options.undoQueue
@@ -237,32 +310,188 @@ class CustomCanvas @JvmOverloads constructor(
         if (options.mode != null) config.mode = options.mode
         if (options.bgColor != null) config.bgColor = options.bgColor
         if (options.strokeColor != null) config.strokeColor = options.strokeColor
+
+        if (options.undoQueue != null || options.redoQueue != null) {
+            //se fuerza a redibujar la escena, ya que se han modificado los historiales
+            invalidate()
+        }
+        else {
+            //no es necesario redibujar, pero se necesita actualizar el usedPaint
+            updatePaint()
+        }
+    }
+
+
+    /**
+     * Modifica el color del fondo y agrega un Action especial a la cola de deshacer.
+     */
+    fun tintBackground (newColor: Int) {
+        //se almacenan los valores actuales
+        var prevColor = config.bgColor
+        var prevMode = config.mode
+        //se cambian los del archivo de configuración a los nuevos
+        config.mode = Mode.FILL_BG
+        config.bgColor = newColor
+        //se agrega una acción generada a la cola de deshacer
+        config.undoQueue!!.add(generateActionFromConfig(prevColor))
+        //se devuelve el estado del modo de la configuración al previo
+        config.mode = prevMode
+    }
+
+
+    /**
+     * Deshace una acción.
+     *
+     * La acción deshecha es añadida a la cola de rehacer.
+     */
+    fun undoAction () {
+        if (config.undoQueue!!.isNotEmpty()) {
+            config.redoQueue!!.addLast(config.undoQueue!!.removeLast())
+        }
+    }
+
+
+    /**
+     * Rehace una acción.
+     *
+     * La acción rehecha es añadida a la cola de deshacer.
+     */
+    fun redoAction() {
+        if (config.redoQueue!!.isNotEmpty()) {
+            config.undoQueue!!.addLast(config.redoQueue!!.removeLast())
+        }
+    }
+
+
+    /**
+     * Deshace todo el historial de dibujo, pero deja el fondo del último color seleccionado.
+     *
+     */
+    fun clearDrawings() {
+        config.undoQueue!!.clear()
+        config.redoQueue!!.clear()
     }
 
 
 
-
+    /**
+     * Actualiza los valores de [usedPaint] de acuerdo a los valores de [a] y
+     * el [Mode] que almacena.
+     * @param a El [Action] con los valores a actualizar.
+     */
     private fun updatePaint (a: Action) {
 
         when (a.mode) {
-            Mode.BRUSH -> {
-                usedPaint.color = a.strokeColor!!
-                usedPaint.strokeWidth = a.strokeWidth!!
-            }
+            Mode.BRUSH, Mode.CIRCLE, Mode.RECTANGLE -> updatePaintValues(a.strokeColor, a.strokeWidth, a.isFilled!!)
+
+            Mode.ERASER -> updatePaintValues(config.bgColor, a.strokeWidth)
+
+           else -> return
         }
     }
 
 
+    /**
+     * Método auxiliar para acortar las otras implementaciones de actualización de [usedPaint].
+     *
+     * @param color El valor para [Paint.setColor]. Si se establece null, se usará el color ya existente.
+     * @param strokeWidth El valor para [Paint.setStrokeWidth]. Si se establece a null, se usará el valor ya existente.
+     * @param isFilled El valor para determinar [Paint.Style] según el método [determineStrokeStyle].
+     */
+    private fun updatePaintValues (color: Int? = null, strokeWidth: Float? = null, isFilled: Boolean = false) {
+        if (color != null) usedPaint.color = color
+        if (strokeWidth != null) usedPaint.strokeWidth = strokeWidth
+        usedPaint.style = determineStrokeStyle(isFilled)
+    }
+
+
+    /**
+     * Actualiza los valores de [usedPaint] de acuerdo a los valores guardados en [config]
+     * y el modo en que se encuentra el canvas.
+     *
+     * @param c Un archivo de configuración [ConfigurationOptions] opcional. Por defecto se utiliza [config].
+     */
     private fun updatePaint (c: ConfigurationOptions = config) {
         when (c.mode) {
+            Mode.BRUSH, Mode.RECTANGLE, Mode.CIRCLE -> {
+                updatePaintValues(c.strokeColor!!, c.strokeWidth!!, c.isFilled!!)
+            }
+            Mode.ERASER -> {
+                updatePaintValues(c.bgColor!!, c.strokeWidth!!, c.isFilled!!)
+            }
+            else -> return //no es necesario hacer nada en el resto de casos
+        }
+    }
+
+
+
+    /**
+     * Genera un [Action] en base a los parámetros guardados en [config] y el modo en que se encuentre
+     * actualmente el canvas.
+     *
+     * No todos los modos necesitan la misma configuración, por lo que se evita guardar o crear más de
+     * un objeto Paint al no tener que sobrescribir propiedades innecesarias.
+     *
+     * @param oldBgColor El antiguo color fondo utilizado por la aplicación en caso de que en la configuración
+     * se halle el modo [Mode.FILL_BG].
+     *
+     * Para el resto de [Mode] este valor es nulo.
+     *
+     * @return Un objecto [Action] acorde a los datos del archivo de configuración y el [Mode] en que se encuentra
+     * el canvas.
+     */
+    private fun generateActionFromConfig (oldBgColor: Int? = null) : Action {
+        return when (config.mode!!) {
+
             Mode.BRUSH -> {
-                usedPaint.color = c.strokeColor!!
-                usedPaint.strokeWidth = c.strokeWidth!!
+                Action(
+                    strokeColor = config.strokeColor,
+                    strokeWidth = config.strokeWidth,
+                    mode = config.mode!!,
+                    path = Path(touchPath)
+                )
+            }
+
+            Mode.ERASER -> {
+                Action(
+                    strokeColor = config.bgColor,
+                    strokeWidth = config.strokeWidth,
+                    mode = config.mode!!,
+                    path = Path(touchPath)
+                )
+            }
+
+            Mode.CIRCLE, Mode.RECTANGLE -> {
+                Action(
+                    strokeColor = config.strokeColor,
+                    strokeWidth = config.strokeWidth,
+                    mode = config.mode!!,
+                    path = Path(touchPath),
+                    isFilled = config.isFilled!!
+                )
+            }
+
+            Mode.FILL_BG -> {
+                Action(
+                    strokeColor = config.bgColor,
+                    prevBGColor = oldBgColor
+                )
             }
         }
     }
 
 
+    /**
+     * Devuelve el estilo de trazado necesario según el valor de [isFilled].
+     *
+     * @param isFilled El valor para el que se desea comprobar. Por defecto se utiliza el
+     * valor del isFilled almacenado en [config].
+     *
+     * @return [Paint.Style.FILL_AND_STROKE] si [isFilled] es **true** o [Paint.Style.STROKE] en caso contrario.
+     */
+    private fun determineStrokeStyle(isFilled: Boolean = config.isFilled!!) : Paint.Style {
+        return if (isFilled) Paint.Style.FILL_AND_STROKE else Paint.Style.STROKE
+    }
 
 
 }
